21b97b11956f6561f4647bff36bff9fe3e676099
dfldiego <dfl.diego92@gmail.com>
2021-03-18 11:09:53 -0300

    Heroe component. Ruta con parametro con button

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from 'src/app/services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styles: [
-  ]
+  templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
-  constructor(private _heroesService: HeroesService) { }
+  constructor(
+    private _heroesService: HeroesService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
+  }
+
+  verHeroe(idx: number) {
+    //console.log(idx);
+    this.router.navigate(['heroe', idx]);
   }
 
 }
