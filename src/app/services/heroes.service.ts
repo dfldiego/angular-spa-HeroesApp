a0db1771e67e186ff331a84482bb6529e801d61e
dfldiego <dfl.diego92@gmail.com>
2021-03-18 13:57:45 -0300

    Busqueda. Heroes. Metodo para obtener heroes desde busqueda.

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -70,6 +70,20 @@ export class HeroesService {
     return this.heroes[idx];
   }
 
+  buscarHeroes(termino: string): Heroe[] {
+    let heroesArr: Heroe[] = [];
+    termino = termino.toLowerCase();
+
+    for (const heroe of this.heroes) {
+      let nombre = heroe.nombre.toLowerCase();
+      if (nombre.indexOf(termino) >= 0) {
+        heroesArr.push(heroe);
+      }
+    }
+
+    return heroesArr;
+  }
+
 }
 
 // Para que TS no inserte nueva data que no tenga estas caracteristicas.
