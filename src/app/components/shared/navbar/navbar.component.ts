2a27ffbdc903338a4448dfe180262b9fe4611bfc
dfldiego <dfl.diego92@gmail.com>
2021-03-18 14:49:16 -0300

    Nueva pagina con resultado de busqueda.

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,13 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  // redireccionar del navbar al buscador
   buscarHeroe(termino: string) {
-    console.log(termino);
+    /* console.log(termino); */
+    this.router.navigate(['/buscar', termino]);
   }
 
 }
