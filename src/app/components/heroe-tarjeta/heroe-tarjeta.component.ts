b1a2140500322cdcd78213fec387bd8aafa21737
dfldiego <dfl.diego92@gmail.com>
2021-03-19 13:42:37 -0300

    uso de @output. info del hijo al padre.

--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,15 +10,19 @@ export class HeroeTarjetaComponent implements OnInit {
   // ingreso de heroe desde html padre
   @Input() heroe: any = {};
   @Input() index: number;
+  @Output() heroeSeleccionado: EventEmitter<number>;  // number -> index
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.heroeSeleccionado = new EventEmitter();
+  }
 
   ngOnInit(): void {
   }
 
   verHeroe() {
-    console.log(this.index);
-    this.router.navigate(['/heroe', this.index])
+    /* console.log(this.index);
+    this.router.navigate(['/heroe', this.index]) */
+    this.heroeSeleccionado.emit(this.index);
   }
 
 }
